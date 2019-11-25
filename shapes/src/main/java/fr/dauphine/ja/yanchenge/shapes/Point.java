e031d44dbfc4373c74cb1d664b137d152e569e3a
yanch18 <56257963+yanch18@users.noreply.github.com>
2019-11-25 03:01:40 +0100

    nouvelle version

--- shapes/src/main/java/fr/dauphine/ja/yanchenge/shapes/Point.java
+++ shapes/src/main/java/fr/dauphine/ja/yanchenge/shapes/Point.java
@@ -1,42 +1,77 @@
 package fr.dauphine.ja.yanchenge.shapes;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
  */
-public class Point 
-{   
-    private int x,y;
-    
-    
-	
-  
-    public int getX() {
+public class Point {
+	private int x, y;
+	private static int nbCmpt;
+
+	public int getX() {
 		return x;
 	}
-
-
-
 
 	public int getY() {
 		return y;
 	}
-	  
-  public Point(int x,int y) {
-    	this.x=x;
-        this.y=y;
-    }
 
+	public Point(int px, int py) {
+		this.x = px;
+		this.y = py;
+		nbCmpt++;
 
+	}
 
-	public static void main( String[] args )
-    {  //question 1
-		Point p=new Point(0,0);
-		System.out.println(p.x+" "+p.y);
+	public Point(Point p) {
+		this.x = p.getX();
+		this.y = p.getY();
+		nbCmpt++;
+
+	}
+
+	public boolean isSameAs(Point p) {
+
+		if ((this.x == p.x) && (this.y == p.y)) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public static void main(String[] args) { // Exercice 1
+		/*
+		 * Point p=new Point(0,1); System.out.println(p.x+" "+p.y);
+		 * 
+		 * Point p1 = new Point(5,2); System.out.println(p1.x+" "+p1.y);
+		 * 
+		 * System.out.println( "on a  " + nbCmpt + " points" );
+		 */
+
+		// System.out.println( "Hello World!" );
+
+		// Exercice 2
+		Point p1 = new Point(1, 2);
+		Point p2 = p1;
+		Point p3 = new Point(1, 2);
+		
+	
+	        System.out.println(p1==p2);
+	        System.out.println(p1==p3);
+	       
+	        System.out.println(p1.isSameAs(p2));
+	        System.out.println(p1.isSameAs(p3));
+		
+	
 		
 		
 		
-		
-        System.out.println( "Hello World!" );
-    }
+		ArrayList<Point>list =new ArrayList <Point> ();
+		list.add(p1);
+		System.out.println(list.indexOf(p2));
+		System.out.println(list.indexOf(p3));
+	}
 }
--- shapes/src/main/java/fr/dauphine/ja/yanchenge/shapes/TestPoint.java
+++ shapes/src/main/java/fr/dauphine/ja/yanchenge/shapes/TestPoint.java
@@ -1,18 +1,19 @@
 package fr.dauphine.ja.yanchenge.shapes;
 
 public class TestPoint {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 	
-		
-		
-		/*	//question 2
-		Point p=new Point(0,0);
-		System.out.println(p.x+" "+p.y);
-		*/
 	
+	
+	
+	
+	public static void main( String[] args )
+    {  //question 1
+		//Point p=new Point(0,0);
+		//System.out.println(p.x+" "+p.y);
 		
-	}
-
+		
+		
+		
+        //System.out.println( "Hello World!" );
+    }
 }
